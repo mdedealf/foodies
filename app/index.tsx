7b6592ec9192053b323f2843f9aeeaebc4adba01
mdedealf <dede.alfaruqq@gmail.com>
2025-07-09 11:33:30 +0700

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-green-100">
+      <Text className="text-xl font-bold text-red-100">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          100: "#D33B0D",
+        },
+        green: {
+          100: "#0F9D58",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
