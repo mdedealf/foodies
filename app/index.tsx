69fd7fa44fc7a350610145aa8f68326e00a05eff
Muhammad Dede Alfaruq <dede.alfaruqq@gmail.com>
2025-07-09 15:31:37 +0700

    Feat(homescreen) (#2)
    * feat: adding homescreen view
    
    * feat : add cart button and delivery button

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,16 @@
 import { images, offers } from "@/constants";
 import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CartButton from "@/components/CartButton";
 
 import "./globals.css";
 
@@ -60,6 +68,23 @@ export default function Index() {
           );
         }}
         contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-row items-center justify-center gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Indonesia</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <CartButton />
+          </View>
+        )}
       />
     </SafeAreaView>
   );
