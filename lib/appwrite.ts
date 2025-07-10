5b1508d625815cdfc7ef7c451c628973a05a4193
Muhammad Dede Alfaruq <dede.alfaruqq@gmail.com>
2025-07-10 15:19:00 +0700

    feat/auth : add appwrite sign up and sign in function and sign in sign up page (#4)

---
+++ lib/appwrite.ts
@@ -0,0 +1,53 @@
+import { CreateUserParams, SignInParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
+
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
+  platform: "com.alfa.foodies",
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DB_ID!,
+  userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID!,
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+const avatars = new Avatars(client);
+
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: CreateUserParams) => {
+  try {
+    const newAccount = await account.create(ID.unique(), email, password, name);
+    if (!newAccount) throw new Error("Failed to create user account");
+
+    await signIn({ email, password });
+
+    const avatarUrl = avatars.getInitialsURL(name);
+
+    return await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      { email, name, accountId: newAccount.$id, avatar: avatarUrl }
+    );
+  } catch (e: string | any) {
+    throw new Error(e?.message || "Unknown error occurred while creating user");
+  }
+};
+
+export const signIn = async ({ email, password }: SignInParams) => {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+  } catch (e: string | any) {
+    throw new Error(e?.message || "Failed to sign in");
+  }
+};
